b14206b4126760d28b53a5101c9d26534da2ab3d
obolentsevB <obolencev.bogdan@lll.kpi.ua>
2026-02-22 10:29:44 +0200

    Implement parallel column sum computation

--- PO_Lab1.cpp
+++ PO_Lab1.cpp
@@ -1,43 +1,75 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <thread>
 #include <random>
 
 using namespace std;
 using namespace std::chrono;
 
-int main() {
-
-    const int n = 2000; 
-    vector<vector<double>> matrix(n, vector<double>(n));
-
-    // Initialize random number generator
-    mt19937 gen(42);
-    uniform_real_distribution<> dis(0, 100);
-
-    // Fill matrix with random numbers
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            matrix[i][j] = dis(gen);
-        }
-    }
-
-    // Measure sequential execution time
-    auto start = high_resolution_clock::now();
-
-    //Place the sum of column elements on the main diagonal
-    for (int j = 0; j < n; ++j) {
+void compute_columns(int start_col, int end_col, int n, vector<vector<double>>& matrix) {
+    for (int j = start_col; j < end_col; ++j) {
         double sum = 0;
         for (int i = 0; i < n; ++i) {
             sum += matrix[i][j];
         }
         matrix[j][j] = sum; 
     }
+}
 
-    auto end = high_resolution_clock::now();
-    duration<double> diff = end - start;
+int main() {
+    int n, num_threads;
+    cout << "Enter matrix size (n): ";
+    cin >> n;
 
-    cout << "Sequential Time: " << diff.count() << " s" << endl;
+    cout << "Enter number of threads for parallel execution: ";
+    cin >> num_threads;
+
+    vector<vector<double>> matrix(n, vector<double>(n));
+    mt19937 gen(42);
+    uniform_real_distribution<> dis(0, 100);
+
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix[i][j] = dis(gen);
+        }
+    }
+
+    auto s_start = high_resolution_clock::now();
+    
+    for (int j = 0; j < n; ++j) {
+        double sum = 0;
+        for (int i = 0; i < n; ++i) { 
+            sum += matrix[i][j]; 
+        }
+        matrix[j][j] = sum;
+    }
+    
+    auto s_end = high_resolution_clock::now();
+    duration<double> s_diff = s_end - s_start;
+    cout << "Sequential Time: " << s_diff.count() << " s" << endl;
+
+    vector<thread> threads;
+    int cols_per_thread = n / num_threads;
+
+    auto p_start = high_resolution_clock::now();
+    
+    for (int t = 0; t < num_threads; ++t) {
+        int start_col = t * cols_per_thread;
+        int end_col = (t == num_threads - 1) ? n : (t + 1) * cols_per_thread;
+        threads.emplace_back(compute_columns, start_col, end_col, n, ref(matrix));
+    }
+
+    for (auto& th : threads) {
+        th.join();
+    }
+    
+    auto p_end = high_resolution_clock::now();
+    duration<double> p_diff = p_end - p_start;
+
+    cout << "Parallel Time (" << num_threads << " threads): " << p_diff.count() << " s" << endl;
+    cout << "Speedup: " << s_diff.count() / p_diff.count() << "x" << endl;
 
     return 0;
 }
